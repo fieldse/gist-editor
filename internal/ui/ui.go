60571cf6f8d51ec5dd2095d068bb9be9bb95962c
Matt <mattfields+dev@pm.me>
2023-11-03 16:46:56 +1000

    add basic layout with buttons

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -8,13 +8,38 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+func viewGists() {
+	// TODO
+}
+
+func newGist() {
+	// TODO
+}
+
 func Startui() {
 	a := app.New()
-	w := a.NewWindow("Gist editor")
-	title := widget.NewLabel("Gist editor")
-	text := widget.NewLabel("Hello Gist!")
-	content := container.New(layout.NewVBoxLayout(), title, text)
+	w := a.NewWindow("GistEdit")
 	w.Resize(fyne.NewSize(600, 400))
+
+	// Title
+	title := widget.NewLabel("Welcome to the Gist editor!")
+	title.TextStyle.Bold = true
+	subLabel := widget.NewLabel("Click View Gists or New Gist below.")
+
+	// Title and welcome text container
+	titleContainer := container.NewVBox(title, subLabel)
+
+	// Buttons for "View Gists" and "New Gist"
+	b1 := widget.NewButton("View Gists", viewGists)
+	b2 := widget.NewButton("New Gist", newGist)
+
+	// Centered buttons grid
+	buttons := container.NewGridWithColumns(2, b1, b2)
+
+	spacer := layout.NewSpacer()
+
+	// Vertical grid layout
+	content := container.New(layout.NewGridLayoutWithRows(3), titleContainer, spacer, buttons)
 
 	w.SetContent(content)
 	w.ShowAndRun()
