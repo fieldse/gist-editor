b2bfbd1fd09c42964542f49f712267f6d82a485e
Matt <mattfields+dev@pm.me>
2023-11-03 16:15:14 +1000

    add basic window and layout

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -1,15 +1,22 @@
 package ui
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func Startui() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Gist editor")
+	title := widget.NewLabel("Gist editor")
+	text := widget.NewLabel("Hello Gist!")
+	content := container.New(layout.NewVBoxLayout(), title, text)
+	w.Resize(fyne.NewSize(600, 400))
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	w.SetContent(content)
 	w.ShowAndRun()
 
 }
