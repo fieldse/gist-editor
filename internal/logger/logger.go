8b6d32ed7290b727516b7adb9edf04b47a3456e4
Matt <mattfields+dev@pm.me>
2023-11-04 17:21:38 +1000

    add logger

---
+++ internal/logger/logger.go
@@ -0,0 +1,21 @@
+package logger
+
+import (
+	"fmt"
+	"log"
+)
+
+// Log info message to console
+func Info(msg string, a ...interface{}) {
+	log.Printf("[info] %s", fmt.Sprintf(msg, a...))
+}
+
+// Log debug message to console
+func Debug(msg string, a ...interface{}) {
+	log.Printf("[debug] %s", fmt.Sprintf(msg, a...))
+}
+
+// Log warn message to console
+func Warn(msg string, a ...interface{}) {
+	log.Printf("[warn] %s", fmt.Sprintf(msg, a...))
+}
