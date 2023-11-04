100e47ba1506891e57ee2239285c20d172023552
Matt <mattfields+dev@pm.me>
2023-11-04 18:35:16 +1000

    working but wonky button layout

--- internal/ui/listview.go
+++ internal/ui/listview.go
@@ -17,17 +17,28 @@ func ListWidget(hide func()) *fyne.Container {
 	okButton := widget.NewButton("ok", hide)
 
 	// Example content widget
-	buttons := container.NewHBox(spacer, okButton)
-	vBox := container.NewVBox(title, spacer, buttons)
+	var data = []string{"a", "string", "list"}
+	list := widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i])
+		})
 
-	content := container.NewHBox(vBox)
+	buttons := container.NewGridWithColumns(3, spacer, spacer, okButton)
+	content := container.NewStack(title, list, buttons)
+
 	return content
 }
 
 // Return a List view window
 func ListWindow(a fyne.App) fyne.Window {
 	w := a.NewWindow("Your Gists")
-	w.Resize(fyne.NewSize(600, 400))
+	w.Resize(fyne.NewSize(500, 300))
 
 	content := ListWidget(w.Hide)
 	w.SetContent(content)
