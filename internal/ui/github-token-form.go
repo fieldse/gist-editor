abed812981c7c8dfa115519009f7e60b3b79a920
Matt <mattfields+dev@pm.me>
2023-11-18 12:01:16 +1000

    add ReadGithubToken func

--- internal/ui/github-token-form.go
+++ internal/ui/github-token-form.go
@@ -16,7 +16,8 @@ import (
 )
 
 // Path under the user's home directory for storing app configuration
-var CONFIG_DIR_NAME = "./gist-editor" // FIXME: should not be hardcoded
+var CONFIG_DIR_NAME = "./gist-editor"                                    // FIXME: should not be hardcoded
+var GITHUB_CONFIG_FILE = path.Join(userConfigPath(), "github-token.txt") // FIXME: Improve file format
 
 func GithubTokenModal(cfg *AppConfig) *dialog.FormDialog {
 	w := cfg.MainWindow.Window
@@ -34,6 +35,12 @@ func GithubTokenModal(cfg *AppConfig) *dialog.FormDialog {
 		if !b {
 			return
 		}
+		err := saveToken(tempVal)
+		if err != nil {
+			dialog.NewError(err, w)
+			return
+		}
+		dialog.NewInformation("Github token saved", "Github token updated", w)
 		logger.Debug("Github API token saved: %v", tempVal)
 		cfg.GithubConfig.GithubAPIToken = tempVal
 	}
@@ -46,10 +53,24 @@ func GithubTokenModal(cfg *AppConfig) *dialog.FormDialog {
 	return d
 }
 
+// ReadGithubToken reads and returns the Github API token, if it exists.
+func ReadGithubToken() (string, error) {
+	if !fileExists(GITHUB_CONFIG_FILE) {
+		return "", nil
+	}
+	token, err := readToken()
+	if err != nil {
+		logger.Error("read github config file failed", err)
+		return "", err
+	}
+	return token, nil
+}
+
 // saveToken saves the Github token to a local file
 func saveToken(token string) error {
-	fp := githubConfigPath()
-
+	if token == "" {
+		return fmt.Errorf("token is empty")
+	}
 	// Ensure the user config dir exists
 	configDir := userConfigPath()
 	if !dirExists(configDir) {
@@ -58,39 +79,35 @@ func saveToken(token string) error {
 			return fmt.Errorf("create user config dir failed: %w", err)
 		}
 		logger.Info("created user config directory at %s", configDir)
-
 	}
 	// Save the token to file
 	data := []byte(token)
-	err := os.WriteFile(fp, data, 0644)
+	err := os.WriteFile(GITHUB_CONFIG_FILE, data, 0644)
 	if err != nil {
 		return fmt.Errorf("save token to file failed: %w", err)
 	}
-	logger.Info("saved Github token: %s", fp)
+	logger.Info("saved Github token: %s", GITHUB_CONFIG_FILE)
 	return nil
 }
 
 // readToken reads the Github token from a local file
 // returns error on empty
 func readToken() (string, error) {
-	fp := githubConfigPath()
-	if !fileExists(fp) {
-		return "", fmt.Errorf("config file not found: %s", fp)
+	if !fileExists(GITHUB_CONFIG_FILE) {
+		return "", fmt.Errorf("config file not found: %s", GITHUB_CONFIG_FILE)
 	}
-	f, err := os.Open(fp)
+	f, err := os.Open(GITHUB_CONFIG_FILE)
 	if err != nil {
-		return "", fmt.Errorf("error reading file: %s -- err: %s", fp, err)
+		return "", fmt.Errorf("error reading file: %s -- err: %s", GITHUB_CONFIG_FILE, err)
 	}
 	defer f.Close()
 
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(f)
 	return buf.String(), nil
-
 }
 
 // fileExists checks if a file exists at the given path.
-// Returns error if it's a directory
 func fileExists(fp string) bool {
 	data, err := os.Stat(fp)
 	return err == nil && !data.IsDir()
@@ -100,11 +117,6 @@ func fileExists(fp string) bool {
 func dirExists(fp string) bool {
 	data, err := os.Stat(fp)
 	return err == nil && data.IsDir()
-}
-
-// githubConfigPath returns the path to the github token config file
-func githubConfigPath() string {
-	return path.Join(userConfigPath(), "github-token.txt") // FIXME: Improve file format
 }
 
 // userConfigPath returns the save path for config settings
