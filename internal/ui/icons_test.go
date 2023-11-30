2e4fc273b5a2b89ab1734923388beebf14a1ed26
Matt <mattfields+dev@pm.me>
2023-11-30 15:50:40 +1000

    add stretchr/testify package; add icons_test

---
+++ internal/ui/icons_test.go
@@ -0,0 +1,14 @@
+// tests for icons loading functions
+package ui
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_loadIcon(t *testing.T) {
+	// TODO
+	assert.Equalf(t, true, false, "expected foo")
+
+}
