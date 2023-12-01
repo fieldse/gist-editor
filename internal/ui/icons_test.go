a8012d3f8dd78798f904b5085e1691251e9ac496
Matt <mattfields+dev@pm.me>
2023-12-01 13:17:36 +1000

    use placeholder theme icon

--- internal/ui/icons_test.go
+++ internal/ui/icons_test.go
@@ -7,12 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var exampleIconNames = []string{"bold.png", "italic.png", "quote-block.png", "undo.png", "h1.png"}
-
 func Test_loadIcon(t *testing.T) {
-	for _, iconName := range exampleIconNames {
-		data, err := loadIcon(iconName)
-		assert.Nilf(t, err, "load icon file should succeed: %s", iconName)
-		assert.NotEmptyf(t, data, "icon data should not be empty for %s", iconName)
-	}
+	icons, err := ToolbarIcons{}.Load()
+	assert.Nilf(t, err, "load icon data")
+	assert.NotEmptyf(t, icons.BoldIcon.Content(), "icon data should not be empty")
 }
