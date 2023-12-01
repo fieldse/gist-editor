6c46a4527b319591d1a2f6913e9f1f954ed3e2c0
Matt <mattfields+dev@pm.me>
2023-12-01 22:54:57 +1000

    placeholder tests

--- internal/editor/editor-funcs_test.go
+++ internal/editor/editor-funcs_test.go
@@ -11,3 +11,15 @@ func TestReplaceWithFoo(t *testing.T) {
 	new := replaceWithFoo("some text")
 	assert.Equalf(t, "foo", new, "should be replaced with 'foo'")
 }
+
+func Test_selectionToBold(t *testing.T) {
+	// TODO
+}
+
+func Test_toLines(t *testing.T) {
+	// TODO
+}
+
+func Test_replaceChunk(t *testing.T) {
+	// TODO
+}
