5df8a6ceba30a4695025a6828e32deb2434da993
Matt <mattfields+dev@pm.me>
2023-12-01 19:49:50 +1000

    break out preview/editor into separate component

--- internal/ui/editor.go
+++ internal/ui/editor.go
@@ -12,17 +12,13 @@ import (
 // Editor represents the Gist editor window, and provides methods
 // to update the title & content of the editor widget
 type Editor struct {
-	Title               string
-	Content             string
-	IsDirty             bool
-	editor              *widget.Entry    // the text editor field
-	editWindow          fyne.Window      // the editor window
-	singlePane          *fyne.Container  // the editor as single-pane view
-	splitPane           *container.Split // the editor as split-pane view with preview
-	splitOrSinglePane   *fyne.Container  // a container which either contains the split view or single view
-	togglePreviewButton *widget.Button
-	IsVisible           bool
-	PreviewVisible      bool // is the preview pane visible, or are we in single pane view
+	Title                string
+	Content              string
+	IsDirty              bool
+	editor               *widget.Entry         // the text editor field
+	editWindow           fyne.Window           // the editor window
+	previewEditContainer *PreviewEditContainer // a wrapper, containing the preview and edit widgets
+	IsVisible            bool
 }
 
 // Show displays the editor window
@@ -35,20 +31,6 @@ func (e *Editor) Show() {
 func (e *Editor) Hide() {
 	e.IsVisible = false
 	e.editWindow.Hide()
-}
-
-// TogglePreview toggles visibility of the markdown preview pane
-func (e *Editor) TogglePreview() {
-	if !e.PreviewVisible { // Hide single-pane view, show split-pane
-		e.splitOrSinglePane = container.NewHBox(e.splitPane)
-		e.togglePreviewButton.SetText("Hide Preview")
-		e.PreviewVisible = true
-	} else { // Hide split-pane view, show single-pane
-		e.splitOrSinglePane = container.NewHBox(e.singlePane)
-		e.togglePreviewButton.SetText("Show Preview")
-		e.PreviewVisible = false
-	}
-	e.Show()
 }
 
 // SetContent sets the contents of the text editor field
@@ -69,26 +51,20 @@ func (e Editor) New(cfg *AppConfig) *Editor {
 	f := cfg.CurrentFile
 	w.Resize(fyne.NewSize(800, 600))
 
-	content, editor, singlePane, splitPane, splitOrSinglePane, togglePreviewButton := editUI(cfg, f.Gist, w)
+	content, editor, previewEditContainer := editUI(cfg, f.Gist, w)
 	w.SetContent(content)
 	w.CenterOnScreen()
 
 	return &Editor{
-		editor:              editor,
-		editWindow:          w,
-		splitPane:           splitPane,
-		singlePane:          singlePane,
-		splitOrSinglePane:   splitOrSinglePane,
-		togglePreviewButton: togglePreviewButton,
+		editor:               editor,
+		editWindow:           w,
+		previewEditContainer: previewEditContainer,
 	}
 }
 
 // Generates the UI for the edit window
-// Returns the container, and a pointer to the content editor, single-pane and split-pane containers,
-// a container to hold them both, and the toggle preview button
-func editUI(cfg *AppConfig, g *github.Gist, w fyne.Window) (*fyne.Container, *widget.Entry, *fyne.Container, *container.Split,
-	*fyne.Container,
-	*widget.Button) {
+// Returns the container, and a pointer to the content editor, and a wrapper for the single-pane and split-pane containers,
+func editUI(cfg *AppConfig, g *github.Gist, w fyne.Window) (*fyne.Container, *widget.Entry, *PreviewEditContainer) {
 
 	// Title
 	titleBox := TitleBox(g.Filename)
@@ -109,20 +85,11 @@ func editUI(cfg *AppConfig, g *github.Gist, w fyne.Window) (*fyne.Container, *wi
 	previewPane := container.NewBorder(widget.NewLabel("Preview"), nil, nil, nil, preview)
 	editor.OnChanged = preview.ParseMarkdown // parse markdown to rich text on changed
 
-	// Split pane view
-	splitView := container.NewHSplit(editPane, previewPane)
-
-	// Single pane (no preview)
-	singlePane := container.NewHBox(editPane)
-
-	// Both split and single views in a container
-	bothPanes := container.NewHBox(singlePane)
+	// Preview and edit pane wrapper
+	previewEditContainer := PreviewEditContainer{}.New(previewPane, editPane)
 
 	// Buttons
 	spacer := layout.NewSpacer()
-	togglePreviewButton := widget.NewButton("Show preview", func() {
-		cfg.Editor.TogglePreview()
-	})
 	saveButton := widget.NewButton("Save", func() {
 		cfg.SaveFile()
 	})
@@ -130,9 +97,57 @@ func editUI(cfg *AppConfig, g *github.Gist, w fyne.Window) (*fyne.Container, *wi
 		cfg.CloseFile()
 		cfg.Editor.Hide()
 	})
-	buttons := ButtonContainer(4, spacer, togglePreviewButton, saveButton, closeButton)
+	buttons := ButtonContainer(4, spacer, previewEditContainer.ToggleButton, saveButton, closeButton)
 
 	// Wrapper container
-	content := container.NewBorder(titleBox, buttons, nil, nil, bothPanes)
-	return content, editor, singlePane, splitView, bothPanes, togglePreviewButton
+	content := container.NewBorder(titleBox, buttons, nil, nil, previewEditContainer.Content)
+	return content, editor, previewEditContainer
+}
+
+// PreviewEditContainer is the container wrapper for the Preview and Edit
+// panes. It alternately displays either the editor or editor with preview
+// in split view
+type PreviewEditContainer struct {
+	Content        *fyne.Container // contains both the preview and edit view
+	ToggleButton   *widget.Button  // the toggle Preview button
+	previewPane    *fyne.Container
+	editPane       *fyne.Container
+	PreviewVisible bool
+}
+
+// New returns a new PreviewEditContainer with toggle functionality
+func (p PreviewEditContainer) New(
+	previewPane *fyne.Container,
+	editPane *fyne.Container,
+) *PreviewEditContainer {
+	pp := &PreviewEditContainer{
+		previewPane: previewPane,
+		editPane:    editPane,
+		Content:     container.NewStack(previewPane, editPane),
+	}
+	pp.ToggleButton = widget.NewButton("Show preview", func() {
+		pp.TogglePreview()
+	})
+	// We don't show the preview pane by default
+	previewPane.Hide()
+
+	return pp
+}
+
+// TogglePreview toggles the visiblility of the markdown preview
+func (p *PreviewEditContainer) TogglePreview() {
+	if p.PreviewVisible {
+		// do something
+		// hide the preview
+		p.previewPane.Hide()
+		p.editPane.Show()
+		p.ToggleButton.SetText("Show preview")
+	} else {
+		// do something else
+		// show the preview
+		p.previewPane.Show()
+		p.editPane.Hide()
+		p.ToggleButton.SetText("Hide preview")
+	}
+	p.PreviewVisible = !p.PreviewVisible
 }
